3221f46515c1cbae39157b92a0cdfc87f9e6d1f1
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-22 21:42:16 -0300

    create Word entity

---
+++ backend/src/main/java/com/fmrm/wordle/entities/Word.java
@@ -0,0 +1,63 @@
+package com.fmrm.wordle.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_words")
+public class Word {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	// VALIDAÇÃO DE TAMANHO
+	private String name;
+
+	public Word() {
+
+	}
+
+	public Word(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
