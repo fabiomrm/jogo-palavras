0b3cf2c5ae78ade9188c2cf3dc01328f22e3c4e4
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-22 21:35:37 -0300

    create backend project

---
+++ backend/src/main/java/com/fmrm/wordle/FmrmWordleApplication.java
@@ -0,0 +1,13 @@
+package com.fmrm.wordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmrmWordleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmrmWordleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fmrm/wordle/FmrmWordleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmrm.wordle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmrmWordleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
