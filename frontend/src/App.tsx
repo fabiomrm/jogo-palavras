97f44bd040c86851bb94d4c89275b48272b0937d
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-24 08:24:11 -0300

    create context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes } from 'components/Routes';
+import { AuthContextProvider } from 'contexts/AuthContext';
 import { GameContextProvider } from 'contexts/GameContext';
 
 function App() {
   return (
-    <GameContextProvider>
-      <Routes />
-    </GameContextProvider>
+    <AuthContextProvider>
+      <GameContextProvider>
+        <Routes />
+      </GameContextProvider>
+    </AuthContextProvider>
   );
 }
 
--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,2 +1,43 @@
-import { createContext } from 'react';
+import { Children, createContext, useContext, useState } from 'react';
 import { Role } from 'types';
+import { TokenData } from 'utils/token';
+
+export type AuthContextData = {
+  authenticated: boolean;
+  tokenData?: TokenData;
+  roles?: Role[];
+};
+
+export type AuthContextType = {
+  authContextData: AuthContextData;
+  setAuthContextData: (authContextData: AuthContextData) => void;
+};
+
+export const AuthContext = createContext<AuthContextType>({
+  authContextData: {
+    authenticated: false
+  },
+  setAuthContextData: () => null
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+export const AuthContextProvider = ({ children }: Props) => {
+  const [authContextData, setAuthContextData] = useState<AuthContextData>({
+    authenticated: false
+  });
+
+  const value = {
+    authContextData,
+    setAuthContextData
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const auth = useContext(AuthContext);
+
+  return auth;
+};
